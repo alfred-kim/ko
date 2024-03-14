014c748b0d6ace7672c13b4f0147e6cf76de620d
Alex <sanchezaleko001@gmail.com>
2024-03-14 13:16:47 +0300

    First commit

---
+++ yyy/src/Main.kt
@@ -0,0 +1,18 @@
+fun main(){
+    operators()
+}
+
+fun operators(){
+    var physics=90
+    var biology=87
+    var maths=90
+    var modulus = physics%biology
+    println("the answer is $modulus")
+
+    var computer=33
+    var english=44
+    var kiswahili=99
+    var divide=kiswahili/computer
+    println("the answer is $divide")
+}
+
